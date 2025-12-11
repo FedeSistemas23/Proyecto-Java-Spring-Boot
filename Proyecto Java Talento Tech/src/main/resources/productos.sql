2ce380e73c11e93c9a2274b1f58acc04cfe4a026
FedeSistemas23 <fededelveliz2017@gmail.com>
2025-12-11 08:32:28 -0300

    carga de productos para probar con H2, funciona bien

--- Proyecto Java Talento Tech/src/main/resources/productos.sql
+++ Proyecto Java Talento Tech/src/main/resources/productos.sql
@@ -1,1 +1,5 @@
-"INSERT INTO Producto (nombre, tipo, descripcion, imagen, precioregular, preciodescuento, type, familiaolfativa, stock) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+INSERT INTO Producto (nombre, tipo, descripcion, imagen, precioregular, preciodescuento, type, familiaolfativa, stock)
+VALUES ('Perfume A', 'Fragancia', 'Aroma suave', 'imagen1.jpg', 1000, 800, 'EDP', 'Floral', 25);
+
+INSERT INTO Producto (nombre, tipo, descripcion, imagen, precioregular, preciodescuento, type, familiaolfativa, stock)
+VALUES ('Perfume B', 'Fragancia', 'Aroma intenso', 'imagen2.jpg', 1200, 900, 'EDT', 'Amaderada', 15);
